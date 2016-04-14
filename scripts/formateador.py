15685455411d1df5f3dff62d817fb4bfaee64957
Luis <Luis>
2016-04-14 16:53:03 -0600

    agregar lector notas para las obs

--- scripts/formateador.py
+++ scripts/formateador.py
@@ -2,11 +2,14 @@
 import subprocess
 import sys
 import os
+from lector_notas import LectorNotasCSV
 
 #Get the path to the main dir
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 subprocess.call(["unoconv", "-f", "csv", "-e", "FilterOptions=44,34,76", "-o", "/tmp/listado.csv", "%s"%os.path.join(BASE_DIR, 'Listado.ods')])
+
+ln = LectorNotasCSV('/tmp/listado.csv')
 
 with open("/tmp/listado.csv", 'r') as archivo:
     ll = archivo.readlines()
@@ -14,9 +17,16 @@ with open("/tmp/listado.csv", 'r') as archivo:
 notas = open(os.path.join(BASE_DIR, 'FormatoNotas/notas.cvs'),'w')
 
 ltex = "%% ESTE ES EL ARCHIVO PARA LA TABLA \n"
-for l in ll[:-1]:
+for ind,l in enumerate(ll[:-1]):
     lsep = l.split(',')
-    ltex += '&'.join([c for k,c in enumerate(lsep) if k not in [3,8]]) + "\\\\ \n \\hline \n"
+    if ind > 0:
+        obs = ln.observaciones(ind - 1)[0] 
+    else:
+        obs = 'Obs'
+
+    ltex += '&'.join([c for k,c in enumerate(lsep) if k not in [3,8]]) +\
+            '&' + obs + "\\\\ \n \\hline \n"
+
 notas.write(ltex)
 
 
---
+++ scripts/lector_notas.py
@@ -0,0 +1,186 @@
+import csv
+import pandas as pd
+#Define una clase que toma un archivo abierto
+
+class LectorNotasCSV():
+    def __init__(self,archivo):
+        #Lee la cadena o objeto archivo 
+        ## la columna Cuenta se tiene que leer como cadena
+        self.df = pd.read_csv(archivo,dtype={'Cuenta':str})
+
+        #convierte el DataFrame en diccionarios de cada fila
+        #ej. {'Correo Electronico': 'luisberlioz@gmail.com',
+#         'Cuenta': nan,
+#         'Exa 1': 48.0,
+#         'Exa 2': 48.0,
+#         'Nombre Completo': 'PAUTA',
+#         'Nº': 12,
+#         'Tarea 1': 5.0,
+#         'Tarea 2': 5.0,
+#         'Tarea 3': 5.0,
+#         'Tarea 4': 5.0,
+#         'Tarea 5': 5.0,
+#         'Tarea 6': 5.0,
+#         'Tarea 7': 5.0}
+
+        self.Dlist = self.df.to_dict('records')
+        self.encabezados = list(self.df.columns)
+
+
+    def notas(self,examen):
+        """
+        Retorna una lista (sin ningun orden en especifico)
+        de todas las notas validas. Para fines estadisticos
+
+        examen: cadena con el nombre de la columna
+        """
+        salida = []
+        for l in self.Dlist:
+            try:
+                if l['Nombre Completo'] != 'PAUTA': 
+                    notita = float(l[examen])
+                    if notita > 0:
+                        salida.append(notita)
+            except ValueError:
+                pass
+        return salida
+
+    def list_email_addresses(self):
+        salida = []
+        for l in self.Dlist:
+            salida.append(l['Correo Electronico'])
+        return salida
+
+
+    def correos_validos(self,examen,**kwargs):
+        """
+        Retorna una lista (sin ningun orden en especifico)
+        de todas las notas validas. Para fines estadisticos
+
+        examen: cadena con el nombre de la columna
+        """
+        campo = kwargs.get('campo','Correo Electronico')
+        salida = []
+        for l in self.Dlist:
+            if self.es_buen_correo(l,examen):
+                salida.append(l[campo])
+        return salida
+
+    def buscar_por_campo(self,direc,*args,**kwargs):
+        '''
+        retorna diccionario con la informacion de alumno
+        dada una direccion de correo
+
+        pn: retorna el primer nombre
+        '''
+        campo = kwargs.get('campo','Correo Electronico')
+        for l in self.Dlist:
+            if l[campo] == direc:
+                if 'pn' in args:
+                    return l['Nombre Completo'].split()[0]
+                elif 'em' in args:
+                    return l['Correo Electronico'].split()[0]
+                else:
+                    ##QUITA LOS ESPACIOS DE LOS KEYS PARA USAR CON CONTEXT DJANGO
+                    es_para_context = kwargs.get('context',False)
+                    if es_para_context:
+                        return { ''.join(k.split()):l[k] for k in l }
+                    else:
+                        return l
+        else:
+            if 'pn' in args:
+                return ''
+            elif 'em' in args:
+                return ''
+            else:
+                return {}
+
+    def es_buen_correo(self,D,examen):
+        '''
+        D; es un diccionario de con las claves habituales
+        retorna True si el diccionario pasa todas las pruebas
+        por ejemplo el tiene nombre completo, numero de cuenta
+        el correo pasa cierta prueba; etc.
+
+        examen: es la cadena con el nombre de la columna del examen que hay que revisar.
+        '''
+        nombre = D.get("Nombre Completo",False)
+        if not nombre:
+            return False
+
+        correo = D.get("Correo Electronico",False)
+        if not correo:
+            return False
+
+#Los alumnos de maestria no tienen numero de cuenta
+        if "Cuenta" in self.encabezados:
+            cuenta = D.get("Cuenta",False)
+            if not cuenta:
+                return False
+
+        try:
+            nota = float(D.get(examen,False))
+        except ValueError:
+            return False
+        if not nota and nota<5:
+            return False
+        return True
+
+    def pctl(self,v,examen):
+        '''
+        retorna el percentil de v en relacion a notas
+        '''
+        grades = self.notas(examen)
+        grades.sort()
+        i = 1
+        while grades[i] < v:
+            i += 1
+            if i > (len(grades) - 1):
+                return 100.0
+        return round(abs((i - 0.5)*100/len(grades)))
+
+    def observaciones(self, ind, nota_final="Total"):
+        '''
+        Para un indice, devuelve las observaciones en cuanto a los examenes 
+        '''
+        #Encontramos todas las columnas que parecen examenes
+        exas = [ c for c in self.df.columns if 'exa' in c.lower() ]
+        resul = self.df[exas].iloc[ind]
+        final = self.df[nota_final].iloc[ind]
+
+        #Si hizo al menos un examen se fue chuco
+        if final >= 65:
+            return ('APR', 'aprobado')
+        else: 
+            #numero de resultados positivos
+            nresul = (resul>0).sum()
+            if nresul == 0:
+                return ('NSP', 'no se presento')
+            # si hizo el primer examen
+            elif nresul < len(resul) and resul[0]>0:
+                return ('ABD', 'abandono')
+            else:
+                return ('RPB', 'reprobado')
+
+    def latex_line(self, ind, **kwargs):
+        '''
+        retorna una linea en estilo latex (para la sabana)
+        el argumento 'nota_final' permite controlar la columna
+        que contiene las notas finales. El valor predeterminado es 
+        'Total' 
+
+        not_in: indice no deseados
+        '''
+        fil = list(self.df.iloc[ind].astype(str))
+        obs = kwargs.get('nota_final', False)
+        not_in_list = kwargs.get('not_in', [])
+        if obs:
+            fil.append(self.observaciones(ind, obs)[0])
+
+        return '&'.join([f for k,f in enumerate(fil) if k not in not_in_list])
+
+
+        
+        
+            
+
