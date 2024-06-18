62aabf631a9ff69ee037e2394cba61b796a659d4
Luis <lab156@case.edu>
2024-06-17 20:00:15 -0600

    Mon Jun 17 08:00:15 PM CST 2024

--- scripts/lector_notas.py
+++ scripts/lector_notas.py
@@ -280,7 +280,8 @@ def main():
                 #        .iterrows():
             #print(f"{r[1][1]} <{r[1][2]}>")
         for r in listado.df.iterrows():
-            print(f"{r[1][1]} <{r[1][1]}>")
+            print(f"{r[1][1]} <{r[1][2]}>")
+        print(listado.df['Nombre Completo'])
 
 
 if __name__ == "__main__":
